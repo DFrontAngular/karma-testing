77f78795d459e56d4d63eef461e9567cb4c452d4
Jose Francisco Ruso Alvarez <jrusoalv@emeal.nttdata.com>
2024-09-24 11:20:25 +0200

    feat(overview): create overview component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'overview',
+    component: OverviewComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full',
+  },
+];
